9228b7c88a5dd940a0646aced12326dace73a0cb
webshakil <chumkysk@gmail.com>
2023-03-21 10:43:44 +0600

    initial setup done

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+const head = () => {
+  return (
+    <>
+      <title>The Shakil</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+};
+
+export default head;
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-rose-900'>Page </h1>
+      <p>Some Content</p>
+    </div>
+  );
+};
+
+export default page;
